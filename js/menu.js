9fbbfd2a14f6678ae6e65e72f6c238dc8138d223
Mauro <mauro.spinosi94@gmail.com>
2020-09-21 14:39:49 +0200

    ja parte 2

--- js/menu.js
+++ js/menu.js
@@ -9,10 +9,9 @@ $(document).ready(function() {
   // Data di partenza del calendario
   var date = "2018-01-01";
   var momentDate = moment(date);
-  console.log(momentDate);
 
   // Stampo come titolo il mese selezionato
-  $("#title").text(momentDate.format("MMMM"));
+  $("#title").text(momentDate.format("MMMM YYYY"));
 
   // Stampo il giorno e il mese slezionato nella data di partenza
   var source = $("#month-template").html();
@@ -20,50 +19,54 @@ $(document).ready(function() {
 
   for (var i = 1; i <= momentDate.daysInMonth(); i ++){
 
-    // var date = addZero(i);
-    // var dateComplete = momentDate.format("YYYY") + "-" + momentDate.format("MM")  + "-" + day;
+    var dateComplete =  momentDate.format("YYYY") + "-" +  momentDate.format("MM") + "-" + addZero(i);
 
     var context = {
       "day": i,
       "month": momentDate.format("MMMM"),
-      // "dateComplete" : holiday,
+      "holiday" : dateComplete,
     };
 
     var html = template(context);
+
     $("#mese").append(html);
+
   }
   $.ajax(
     {
-      "url": " https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
-      "method": "GET",
+      "url": " https://flynn.boolean.careers/exercises/api/holidays",
       "data" : {
         "year" : 2018,
-        "month" : 1,
+        "month" : 0,
       },
-      "success": function (data, stato) {
+      "method": "GET",
+      "success": function (data) {
         var resp = data.response;
-        // printHolidays(resp);
+        printHolidays(resp);
       },
       error: function (richiesta, stato, errori) {
         alert("E' avvenuto un errore. " + errore);
       }
     }
   );
+
 });
-//
-// function printHolidays(holidays) {
-//   for(var i=0; i<holidays.length;i++){
-//     var holidayName = holidays[i].name;
-//     var holidayDate = holidays[i].date;
-//
-//     $(".day[data-hol='"+holidayDate+"']").addClass("style");
-//     $(".day[data-hol='"+holidayDate+"'].holidayType").text("-" + holidayName);
-//   }
-// }
-//
-// function addZero(day) {
-//  if(day<0){
-//    return 0 + day;
-//  }
-//  return day;
-// }
+
+//Funzione che colora di rosso i giorni del mese festivi
+function printHolidays(holidays) {
+  for(var i=0; i < holidays.length; i++){
+    var holidayName = holidays[i].name;
+    var holidayDate = holidays[i].date;
+
+    $(".day[data-hol='"+holidayDate+"']").addClass("style");
+    $(".day[data-hol='"+holidayDate+"'] .hol-date").text("-" + " " + holidayName);
+  }
+}
+
+//Funzione che aggiounge lo zero ai numeri < 10
+function addZero (num) {
+  if(num < 10){
+    return "0" + num;
+  }
+  return num;
+}
