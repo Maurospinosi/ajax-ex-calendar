c787f95508b28c9b3d58e18b355252fa3aeca8c3
Mauro <mauro.spinosi94@gmail.com>
2020-09-21 09:19:37 +0200

    js parte 1

--- js/menu.js
+++ js/menu.js
@@ -4,13 +4,45 @@
 // (unici dati disponibili sull’API).
 // Milestone 1
 // Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
+
 $(document).ready(function() {
+  // Data di partenza del calendario
+  var date = "2018-01-01";
+  var momentDate = moment(date);
+  console.log(momentDate);
+
+  // Stampo come titolo il mese selezionato
+  $("#title").text(momentDate.format("MMMM"));
+
+  // Stampo il giorno e il mese slezionato nella data di partenza
+  var source = $("#month-template").html();
+  var template = Handlebars.compile(source);
+
+  for (var i = 1; i <= momentDate.daysInMonth(); i ++){
+
+    // var date = addZero(i);
+    // var dateComplete = momentDate.format("YYYY") + "-" + momentDate.format("MM")  + "-" + day;
+
+    var context = {
+      "day": i,
+      "month": momentDate.format("MMMM"),
+      // "dateComplete" : holiday,
+    };
+
+    var html = template(context);
+    $("#mese").append(html);
+  }
   $.ajax(
     {
       "url": " https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
       "method": "GET",
+      "data" : {
+        "year" : 2018,
+        "month" : 1,
+      },
       "success": function (data, stato) {
-        console.log(data);
+        var resp = data.response;
+        // printHolidays(resp);
       },
       error: function (richiesta, stato, errori) {
         alert("E' avvenuto un errore. " + errore);
@@ -18,3 +50,20 @@ $(document).ready(function() {
     }
   );
 });
+//
+// function printHolidays(holidays) {
+//   for(var i=0; i<holidays.length;i++){
+//     var holidayName = holidays[i].name;
+//     var holidayDate = holidays[i].date;
+//
+//     $(".day[data-hol='"+holidayDate+"']").addClass("style");
+//     $(".day[data-hol='"+holidayDate+"'].holidayType").text("-" + holidayName);
+//   }
+// }
+//
+// function addZero(day) {
+//  if(day<0){
+//    return 0 + day;
+//  }
+//  return day;
+// }
